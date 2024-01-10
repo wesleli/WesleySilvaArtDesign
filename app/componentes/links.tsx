4434e5a498c66a4256f76e1440685b0260b8d72d
wesleli <wesley.ppssilva@gmail.com>
2024-01-09 23:38:56 -0300

    atualizações pontuais 0.2

--- app/componentes/links.tsx
+++ app/componentes/links.tsx
@@ -2,6 +2,9 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+import IconTela01 from "./imagens/Telas_01.png"
+import IconTela02 from "./imagens/Telas_02.png"
 
 export function Links() {
   const pathname = usePathname();
@@ -9,8 +12,8 @@ export function Links() {
   return (
     <nav className='flex items-center justify-between text-black bg-slate-100 h-1/5'>
       <ul className='inline-flex items-center justify-center px-5'>
-        <li><button  type='button' className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3">button 01</button></li>
-        <li><button  type='button'className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3">button 02</button></li>
+        <li><button  type='button' className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold p-2"> <Image src={IconTela01} alt="alt" width={80} height={60} /></button></li>
+        <li><button  type='button'className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold p-2"><Image src={IconTela02} alt="alt" width={80} height={60} /></button></li>
       </ul>
       <ul className='flex items-center justify-center w-1/4 hover:text-yellow-600'>
         <li className={pathname === '/' ? 'active' : ''}>
