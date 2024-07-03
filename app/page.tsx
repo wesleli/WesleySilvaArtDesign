38b33bb25748315002c57d0a79f94377192dd6a5
Wesley Silva <wesley.ppssilva@gmail.com>
2024-07-03 16:13:42 -0300

    atualização de pagina

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import Inicio from "./conteudo-inicial/inicio/inicio"
-import Design from "./conteudo-inicial/design/design"
-import Arte from "./conteudo-inicial/arte/arte"
 import Sobremim from "./conteudo-inicial/sobremim/sobremim"
 import Relogio from "./conteudo-inicial/relogio/relogio"
 import Portfolio from "./conteudo-inicial/portfolio/portfolio"
